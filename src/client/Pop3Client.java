b1eff1994e652790582ae45212b9782a79665356
ARNAUD THOMAS p1411138 <p1411138@univ-lyon1.fr>
2016-03-07 08:37:13 +0100

    add Client package

---
+++ src/client/Pop3Client.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author Thomas Arnaud
+ */
+public class Pop3Client 
+{
+    private Socket socket;
+    private InetAddress ip;
+    private int port;
+    
+    public Pop3Client(InetAddress ip, int port)
+    {
+        this.ip = ip;
+        this.port = port;
+        
+        try {
+            this.socket = new Socket(ip, port);
+        } catch (IOException ex) {
+            Logger.getLogger(Pop3Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
