cd8857e132e4a4cfe92ec99ab86045862433150f
Bruno Buiret <bruno.b@apps-panel.com>
2016-03-07 08:36:16 +0100

    Etats communs de POP3

---
+++ src/common/Pop3State.java
@@ -0,0 +1,14 @@
+package common;
+
+/**
+ * @author Bruno Buiret <bruno.buiret@etu.univ-lyon1.fr>
+ * @author Thomas Arnaud <thomas.arnaud@etu.univ-lyon1.fr>
+ * @author Alexis Rabilloud <alexis.rabilloud@etu.univ-lyon1.fr>
+ */
+public enum Pop3State
+{
+    INITIALIZATION,
+    AUTHORIZATION,
+    TRANSACTION,
+    UPDATE;
+}
