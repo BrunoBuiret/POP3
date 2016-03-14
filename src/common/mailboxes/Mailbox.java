64dbb7da2ee4d5edd9886f68230e0c68f21e76f4
Bruno Buiret <bruno.b@apps-panel.com>
2016-03-14 10:39:11 +0100

    Classe représentant une boîte aux lettres

---
+++ src/common/mailboxes/Mailbox.java
@@ -0,0 +1,11 @@
+package common.mailboxes;
+
+/**
+ * @author Bruno Buiret <bruno.buiret@etu.univ-lyon1.fr>
+ * @author Thomas Arnaud <thomas.arnaud@etu.univ-lyon1.fr>
+ * @author Alexis Rabilloud <alexis.rabilloud@etu.univ-lyon1.fr>
+ */
+public class Mailbox
+{
+    protected String userName;
+}
